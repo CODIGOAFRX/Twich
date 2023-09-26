8449b53d676dff24c2dda58ceb050637735c12bb
CODIGOAFRX <145802023+CODIGOAFRX@users.noreply.github.com>
2023-09-26 16:14:13 +0200

    Add files via upload

--- Twich.java
+++ Twich.java
@@ -19,22 +19,22 @@ public class Twich {
         System.out.println("Hola, bienvenido al canal de cocina con tu yaya, el mejor canal de cocina en streaming");
         
         // definimos variables (con int ya que serán numeros sin decimales ya que no puedes partir un sub)
-        int subs = 3658;
+        int subs = 1200;
         int ganancias ;
         ganancias = subs * 3 ; //definimos la operacion de ganancias
                 
         if (ganancias >= 0 && ganancias <= 1200 ){  //primer if de condición + imprimir mensaje en pantalla
-            System.out.println("bro u broke tus ganancias son " + ganancias + "$" ) ;
+            System.out.println("bro u broke tus ganancias son " + ganancias + "$ mensuales" ) ;
         }
         
         else if (ganancias >= 1201 && ganancias <= 2000 ) {  //primer else if de condición + imprimir mensaje en pantalla
-        System.out.println("bro u into clase media española, tus ganancias son " + ganancias + "$" ) ;
+        System.out.println("bro u into clase media española, tus ganancias son " + ganancias + "$ mensuales" ) ;
         } 
         else if (ganancias >= 2001 && ganancias <= 5000 ) {   //segundo else if de condición + imprimir mensaje en pantalla
-        System.out.println("Ya eres cryptobro, tus ganancias son  " + ganancias + "$" ) ;
+        System.out.println("Ya eres cryptobro, tus ganancias son  " + ganancias + "$ mensuales" ) ;
         } 
         else if (ganancias > 5000 ) {   //tercer else if de condición + imprimir mensaje en pantalla
-        System.out.println("Andorra te espera, tus ganancias son " + ganancias + "$" ) ;
+        System.out.println("Andorra te espera, tus ganancias son " + ganancias + "$ mensuales" ) ;
         } 
         else {  //ultimo else de condición para declarar un error a la hora de introducir datos
             System.out.println("error al introducir datos");
